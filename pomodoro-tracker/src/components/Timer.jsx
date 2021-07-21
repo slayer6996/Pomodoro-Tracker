bd44fcea957f267a9421e461930ab8f4f2515e66
Eshan Hembrom <eshanjan3@gmail.com>
2021-07-21 22:11:47 +0530

    add timer

--- pomodoro-tracker/src/components/Timer.jsx
+++ pomodoro-tracker/src/components/Timer.jsx
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import './styles/timer.css'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
+import { Button } from '@material-ui/core';
 
 function Timer() {
-    const [time, setTime] = useState(0)
+    const [time, setTime] = useState(1500)
     const [start, setStart] = useState(false)
 
     useEffect(() => {
         let interval = null
         if (start) {
             interval = setInterval(() => {
-                setTime(prevTime => prevTime + 1)
+                setTime(prevTime => prevTime - 1)
             }, 1000)
         } else {
             clearInterval(interval)
         }
         return () => clearInterval(interval)
     }, [start])
+
+    useEffect(() => {
+        if (time===0) {
+            setStart(false)
+        }
+    }, [time])
+
     return (
         <>
             <center>
@@ -23,17 +33,23 @@ function Timer() {
                     <center>
                         <div className="time">
                             <div>
-                                <h1>{Math.floor(time / 60)} : {time % 60}</h1>
+                                <h1>{("0" + Math.floor(time / 60)).slice(-2)} : {("0" + time % 60).slice(-2)}</h1>
                             </div>
-                            <button onClick={() => setStart(true)}>start</button>
-                            <button onClick={() => setStart(false)}>stop</button>
-                            <button onClick={() => {
-                                setStart(false)
-                                setTime(0)
-                            }}>reset</button>
+                            {
+                                start ? (<PauseIcon style={{ fontSize: "80" }} onClick={() => setStart(false)} />) :
+                                    (<PlayArrowIcon style={{ fontSize: "80" }} onClick={() => setStart(true)} />)
+                            }
                         </div>
                     </center>
                 </div>
+                {
+                    (time > 0) && (<Button
+                        style={{ backgroundColor: "#ff4b4b", marginTop: "2rem" }}
+                        onClick={() => {
+                            setStart(false)
+                            setTime(1500)
+                        }}>Abort task</Button>)
+                }
             </center>
         </>
     )
